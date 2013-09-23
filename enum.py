2098beb3e697b5d0356b48a91eda4f902abcf5bb
olivecoder <olivecoder@gmail.com>
2013-09-22 23:07:55 -0300

    improving enum.py

---
+++ enum.py
@@ -0,0 +1,27 @@
+class Enum:
+    @classmethod
+    def keys(cls):
+        return [key for key in dir(cls)
+                      if not n.startswith('_') and
+                         not callable(getattr(cls,key))]
+
+    @classmethod
+    def values(cls):
+        return [getattr(cls,key) for key in cls.keys()]
+
+    @classmethod
+    def items(cls):
+        return [(key, getattr(cls,key)) for key in cls.keys()]
+
+    @classmethod
+    def name(cls, value):
+        for k, v in cls.items():
+            if value == v:
+                return k
+
+    def __init__(self, value):
+        self._value = value
+
+    def __str__(self):
+        return self.name(self._value)
+
